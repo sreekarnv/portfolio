e403331a1a6ee1c3ad536a69b89057a65f25ccf6
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-01-24 18:18:44 +0530

    ✨ Add sitemap, partytown & 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-    site: 'https://sreekarnutulapati.vercel.app',
+    site: "https://sreekarnutulapati.vercel.app",
     output: 'static',
     compressHTML: true,
     server: {
         port: 3000
     },
-    integrations: [icon()],
+    integrations: [
+        icon(),
+        sitemap(),
+        partytown({
+            config: {
+                forward: ['dataLayer.push'],
+            },
+        }),
+    ],
     vite: {
         build: {
             cssMinify: true,
